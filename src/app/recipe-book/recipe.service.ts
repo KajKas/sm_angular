9c7c59d27df5f304a3e1832cfb28d51d62e38c6f
kajetan.kasperuk <kajetan.kasperuk@neoteric.eu>
2019-03-05 15:44:21 +0100

    lecture 115

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,19 +1,50 @@
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test recipe', 'test desc', 'https://foodrevolution.org/' +
-      'wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg'),
-    new Recipe('Another test recipe', 'test desc', 'https://foodrevolution.org/' +
-      'wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg'),
-    new Recipe('Yet another test recipe', 'test desc', 'https://foodrevolution.org/' +
-      'wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg')
+    new Recipe(
+      'Test recipe',
+      'test desc',
+      'https://foodrevolution.org/' +
+      'wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg',
+      [
+        new Ingredient('dupa', 2),
+        new Ingredient('cycki', 4)
+      ]
+    ),
+    new Recipe(
+      'Another test recipe',
+      'test desc',
+      'https://foodrevolution.org/wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg',
+      [
+        new Ingredient('dupa', 22),
+        new Ingredient('cycki', 44)
+      ]
+    ),
+    new Recipe(
+      'Yet another test recipe',
+      'test desc',
+      'https://foodrevolution.org/wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg',
+      [
+        new Ingredient('dupa', 222),
+        new Ingredient('cycki', 444)
+      ]
+    )
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
